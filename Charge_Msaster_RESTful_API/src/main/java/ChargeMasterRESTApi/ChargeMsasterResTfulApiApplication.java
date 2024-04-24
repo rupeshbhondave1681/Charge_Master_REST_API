1a721bad41423105b48cf29bc106d013faa1b828
rupesh bhondave <bhondave.rupesh.1681@gmail.com>
2024-04-24 20:15:58 +0530

    Initial commit

---
+++ Charge_Msaster_RESTful_API/src/main/java/ChargeMasterRESTApi/ChargeMsasterResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package ChargeMasterRESTApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeMsasterResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeMsasterResTfulApiApplication.class, args);
+	}
+
+}
---
+++ Charge_Msaster_RESTful_API/src/test/java/ChargeMasterRESTApi/ChargeMsasterResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ChargeMasterRESTApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeMsasterResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
